065a50d1028759e80c11d6a50c0f9b932915f015
Paloma Fontenele <palomafontenele@Palomas-MacBook-Air.local>
2022-08-26 10:11:18 -0400

    Added coding description

--- palindrome_index.py
+++ palindrome_index.py
@@ -1,3 +1,27 @@
+# Given a string of lowercase letters in the range ascii[a-z], determine the index of a character that can be removed to make the string a palindrome. There may be more than one solution, but any will do. If the word is already a palindrome or there is no solution, return -1. Otherwise, return the index of a character to remove.
+
+# Example
+# Either remove 'b' at index  or 'c' at index .
+# Function Description
+# Complete the palindromeIndex function in the editor below.
+# palindromeIndex has the following parameter(s):
+# string s: a string to analyze
+# Returns
+# int: the index of the character to remove or
+# Input Format
+# The first line contains an integer , the number of queries.
+# Each of the next  lines contains a query string .
+
+# Constraints
+# All characters are in the range ascii[a-z].
+# Sample Input
+# STDIN   Function
+# -----   --------
+# 3       q = 3
+# aaab    s = 'aaab' (first query)
+# baa     s = 'baa'  (second query)
+# aaa     s = 'aaa'  (third query)
+
 def palindromeIndex(s):
 
     if s == s[::-1]:
