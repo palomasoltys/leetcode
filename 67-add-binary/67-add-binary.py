14a546255f6f0bf3c420eb7983dc69a6d39ee34a
Paloma Soltys <103966682+palomasoltys@users.noreply.github.com>
2022-10-05 13:18:29 -0400

    Time: 81 ms (7.90%), Space: 13.8 MB (72.59%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,7 +1,18 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        a = int(a, 2)
-        b = int(b, 2)
-        s = a + b
-        return bin(s)[2:]
+        a = a[::-1]
+        b = b[::-1]
+        s = 0
+        for i in range(len(a)):
+            s += int(a[i]) * 2 ** i
+        for i in range(len(b)):
+            s += int(b[i]) * 2 ** i
+        if s == 0:
+            return str(s)
+        res = ""  
+        while s >= 1:
+            res += str(s % 2)
+            s = s//2
+        return res[::-1]
+            
         
