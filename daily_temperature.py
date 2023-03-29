f9c100724f76b689f81d4ab65d420d125eda0956
palomasoltys <pfs601@gmail.com>
2023-03-29 17:15:45 -0400

    optimized solution

--- daily_temperature.py
+++ daily_temperature.py
@@ -10,9 +10,9 @@ def dailyTemperatures(temperatures):
     return res
 
 
-# print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))  # [1,1,4,2,1,1,0,0]
-# print(dailyTemperatures([30, 40, 50, 60]))  # [1,1,1,0]
-# print(dailyTemperatures([30, 60, 90]))  # [1,1,0]
-# print(dailyTemperatures([20, 10, 5]))  # [0,0,0]
+print(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))  # [1,1,4,2,1,1,0,0]
+print(dailyTemperatures([30, 40, 50, 60]))  # [1,1,1,0]
+print(dailyTemperatures([30, 60, 90]))  # [1,1,0]
+print(dailyTemperatures([20, 10, 5]))  # [0,0,0]
 # [8,1,5,4,3,2,1,1,0,0]
 print(dailyTemperatures([89, 62, 70, 58, 47, 47, 46, 76, 100, 70]))
